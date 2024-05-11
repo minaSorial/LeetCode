610e320f243040bce94da3706266a7f09f5b0e64
Mina Sorial <mina.sorial@outlook.com>
2024-05-11 22:07:50 +0300

    second

--- Easy/387. First Unique Character in a String/main.cpp
+++ Easy/387. First Unique Character in a String/main.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+
 
 using namespace std;
 
 class Solution {
 public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> char_map;
+        for (auto ch : s) {
+            char_map[ch] += 1;
+        }
 
+        for (int i = 0; i < s.size();i++) {
+            if (char_map[s[i]] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
 };
 int main() {
 
-    std::cout << "Hello" << std::endl;
+
 
 
     Solution s = Solution();
-
+    string t = "atbb";
+    std::cout << s.firstUniqChar(t) << std::endl;
     return 0;
 }
--- Easy/387. First Unique Character in a String/main.py
+++ Easy/387. First Unique Character in a String/main.py
@@ -2,7 +2,19 @@ from typing import List
 
 
 class Solution:
-    pass
+     def firstUniqChar(self, s):
+        d = {}
+        for i in s:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] += 1
+        
+        for i in range(len(s)):
+            if d[s[i]] == 1:
+                return i
+        return -1
     
 s=Solution()
-
+strin="aabb"
+print(s.firstUniqChar(strin))
